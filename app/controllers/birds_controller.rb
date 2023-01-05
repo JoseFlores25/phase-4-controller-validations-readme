1a14c6986a36e2cf8b19c9a8ec5442b5eef5b201
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2023-01-04 23:32:17 -0500

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,16 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  # rest of controller actions...
+
+  private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
   # GET /birds
   def index
     birds = Bird.all
